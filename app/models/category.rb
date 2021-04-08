81a77769976a2bc5f61eecedb237ba1da7a6c648
Skelman50 <skelman50@gmail.com>
2021-04-09 01:38:15 +0300

    add uniq validate in categories title

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@
 
 class Category < ApplicationRecord
   validates :title, length: { minimum: 3, message: 'Недостатньо символів (необхідно мінімум 3)' }
+  validates :title, uniqueness: { message: "Ім'я категорії має бути унікальним" }
 
   has_many :questions, dependent: :destroy
 end
