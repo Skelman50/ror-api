063759bbb563b9a23fbb7f99a61dedb7ba28fae0
Skelman50 <skelman50@gmail.com>
2021-04-10 07:59:11 +0300

    back to old v

--- app/concepts/category/operation/get_all.rb
+++ app/concepts/category/operation/get_all.rb
@@ -5,10 +5,12 @@ module Category::Operation
     step :get_categories
 
     def get_categories(options, **)
-      options[:response] = Category.left_outer_joins(:questions)
-                                   .select('"categories".*, COUNT(questions.id) as questions_count')
-                                   .order(created_at: :desc)
-                                   .group('categories.id')
+      categories = Category.order(created_at: :desc).all
+      options[:categories] = CategoryPresenters::Collection.new(categories).call
+      # options[:response] = Category.left_outer_joins(:questions)
+      #                              .select('"categories".*, COUNT(questions.id) as questions_count')
+      #                              .order(created_at: :desc)
+      #                              .group('categories.id')
     end
   end
 end
