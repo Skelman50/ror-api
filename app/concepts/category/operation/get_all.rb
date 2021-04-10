d3525ff0dd6a7fbfbb6dc6c25c78f1e092e689b1
Skelman50 <skelman50@gmail.com>
2021-04-10 20:05:35 +0300

    improve get all categories

--- app/concepts/category/operation/get_all.rb
+++ app/concepts/category/operation/get_all.rb
@@ -5,12 +5,12 @@ module Category::Operation
     step :get_categories
 
     def get_categories(options, **)
-      categories = Category.order(created_at: :desc).all
-      options[:response] = CategoryPresenters::Collection.new(categories).call
-      # options[:response] = Category.left_outer_joins(:questions)
-      #                              .select('"categories".*, COUNT(questions.id) as questions_count')
-      #                              .order(created_at: :desc)
-      #                              .group('categories.id')
+      # categories = Category.order(created_at: :desc).all
+      # options[:response] = CategoryPresenters::Collection.new(categories).call
+      options[:response] = Category.left_outer_joins(:questions)
+                                   .select(:categories, 'COUNT(questions.id) as questions_count')
+                                   .order(created_at: :desc)
+                                   .group('categories.id')
     end
   end
 end
